901e36c7f1cee90aa4d1bbde34895bfb5492756e
corsen-olbin <corbinolsen97@gmail.com>
2023-11-24 15:49:22 -0600

    day 1, fix arg parsing

--- lib/advent_of_code_ex/core/days/day1.ex
+++ lib/advent_of_code_ex/core/days/day1.ex
@@ -1,11 +1,23 @@
 defmodule AdventOfCodeEx.Core.Days.Day1 do
 
 
-  def part_1(_input) do
-    :unimplemented
+  def part_1(input) do
+    input
+    |> String.split("", trim: true)
+    |> Enum.reduce(0, fn next, acc -> if next == "(", do: acc + 1, else: acc - 1 end)
   end
 
-  def part_2(_input) do
-    :unimplemented
+  def part_2(input) do
+    input
+    |> String.split("", trim: true)
+    |> calc_until_neg_one
+  end
+
+  def calc_until_neg_one(list), do: calc_until_neg_one_rec(list, 0, 0)
+
+  def calc_until_neg_one_rec(_list, -1, pos), do: pos
+  def calc_until_neg_one_rec([symbol | list], floor, pos) do
+    next_floor = if symbol == "(", do: floor + 1, else: floor - 1
+    calc_until_neg_one_rec(list, next_floor, pos + 1)
   end
 end
